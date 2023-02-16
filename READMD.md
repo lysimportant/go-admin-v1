358ba37e940e0f9cea98d6a7dcb91837694a2ff6
lysimportant <lysimportant@outlook.com>
2023-02-16 21:43:27 +0800

    init: 项目文件初始化

---
+++ READMD.md
@@ -0,0 +1,5 @@
+# 项目搭建
+0. 搭建基本目录
+1. 搭建服务器 gin `go get -u github.com/gin-gonic/gin`
+2. 数据库交互 gorm `go get -u gorm.io/gorm`
+3. 加载配置文件 viper `go get github.com/spf13/viper`
---
+++ router/load_router.go
@@ -0,0 +1,17 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+var Router *gin.Engine
+
+func LoadRouter() {
+	Router = gin.Default()
+	Router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"code": 0,
+			"data": "",
+			"msg":  "现在测试的Gin已正常运行~",
+		})
+	})
+	Router.Run()
+}
